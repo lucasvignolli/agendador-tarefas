3b57af96dad785a0e6c3d0d8e97d12b19d9166e0
Lucas Vignolli <lucas.vignolli@gmail.com>
2026-02-12 11:16:41 -0300

    refatoracao secret key

--- src/main/java/com/lucas/vignolli/agendador_tarefas/infrastructure/security/JwtUtil.java
+++ src/main/java/com/lucas/vignolli/agendador_tarefas/infrastructure/security/JwtUtil.java
@@ -3,25 +3,34 @@ package com.lucas.vignolli.agendador_tarefas.infrastructure.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import java.nio.charset.StandardCharsets;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
 import java.util.Date;
 
 @Service
 public class JwtUtil {
 
-    // Chave secreta usada para assinar e verificar tokens JWT
-    private final String secretKey = "sua-chave-secreta-super-segura-que-deve-ser-bem-longa";
+    @Value("${jwt.secret}")
+    private String secretKey;
 
+//    // Chave secreta usada para assinar e verificar tokens JWT
+//    private static final String secretKey = "c3VhLWNoYXZlLXNlY3JldGEtc3VwZXItc2VndXJhLXF1ZS1kZXZlLXNlci1iZW0tbG9uZ2E=";
 
+    private SecretKey getSecreKey(){
+        byte[] key = Base64.getDecoder().decode(secretKey);
+        return Keys.hmacShaKeyFor(key);
+    }
 
     // Extrai as claims do token JWT (informações adicionais do token)
     public Claims extractClaims(String token) {
         return Jwts.parser()
-                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8))) // Define a chave secreta para validar a assinatura do token
+                .verifyWith(getSecreKey()) // Define a chave secreta para validar a assinatura do token
                 .build()
-                .parseClaimsJws(token) // Analisa o token JWT e obtém as claims
-                .getBody(); // Retorna o corpo das claims
+                .parseSignedClaims(token) // Analisa o token JWT e obtém as claims
+                .getPayload(); // Retorna o corpo das claims
     }
 
     // Extrai o nome de usuário do token JWT
